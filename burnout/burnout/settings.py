48875c39e37e4dd2c506e8a078442f934c73255e
Esteban-Torres <esteban.garcia2295@gmail.com>
2019-11-20 13:21:56 -0500

    Avance 2

--- burnout/burnout/settings.py
+++ burnout/burnout/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
-    'django.contrib.usuario',
+    'burnout.apps.usuario',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
